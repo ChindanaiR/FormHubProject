bafe91321fad1e75ccfb25dcc4984429f24e4323
ChindanaiR <120786823+ChindanaiR@users.noreply.github.com>
2024-03-24 23:51:33 +0700

    some fixes in login/logout page

---
+++ static/authentication/authen.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#logout').addEventListener('click', () => {
+        alert("You Has Successfully Logged Out. See You Next Time! 🥹");
+    });
+});
