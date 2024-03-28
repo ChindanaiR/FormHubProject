1969a588e70b2a6e90f9cda84d55418a8b30288a
punnnnnnnnnn <128019527+punnnnnnnnn@users.noreply.github.com>
2024-03-28 21:18:26 +0700

    Initialize the database

--- form_management/models.py
+++ form_management/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+
+class Point(models.Model):
+    form_size = models.CharField(max_length = 2)
+    point = models.IntegerField()
+    description = models.CharField(max_length = 50)
+    lower_bound = models.IntegerField(max_length = 3)
+    upper_bound = models.IntegerField(max_length = 3)
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length = 30)
+    
+
+class Form(models.Model):
+    owner = models.ForeignKey(User, on_delete = models.CASCADE)
+    form_name = models.CharField(max_length = 125)
+    description = models.CharField(max_length = 450)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    design = models.JSONField()
+    creation_timestamp = models.DateTimeField(auto_now_add = True)
+    publish_timestamp = models.DateTimeField(null = True)
+    is_open = models.BooleanField(default = False)
+    is_sale = models.BooleanField(default = False)
+    form_point = models.ForeignKey(Point, on_delete = models.CASCADE, related_name="form_point")
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from form_management.models import Form
+from authentication.models import User
 
-# Create your models here.
+
+class TransactionCode(models.Model):
+    transaction_code = models.CharField(max_length = 3)
+    description = models.CharField(max_length = 150)
+
+
+class PointTransaction(models.Model):
+    transaction_id = models.ForeignKey(TransactionCode, on_delete = models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    point = models.IntegerField()
+    form_id = models.ForeignKey(Form, on_delete = models.CASCADE, null = True)
